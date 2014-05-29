3daad4ff1d7e5049e0bb75890f544404e4c5b882
raunakb94 <raunakbhansali.nitdgp@gmail.com>
2014-05-29 21:29:38 +0530

    Initial Structure WIthout any Addons!!!

---
+++ src/com/adapt/apptrack/login.java
@@ -0,0 +1,38 @@
+package com.adapt.apptrack;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * 
+     */
+    public login() {
+        super();
+    }
+
+	/**
+	 * 
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 *
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
